d6dd94770cd2b74314c8e3fa1ecb2d69a7560520
Hack Reactor Student <student@hackreactor.com>
2017-04-10 21:19:17 -0700

    GOT SOME TEST TO PASS

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,27 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+
   initialize: function() {
-  }
+  	console.log(this);
+  	this.on('add', function() {
+  		if (this.length < 2) {
+  			this.playFirst();
+  		}
+  	}, this)
+
+  	this.on('ended', function() {
+  		if (this.models.length > 1) {
+
+  		this.shift();
+  		this.playFirst();
+  	}
+  	}, this)
+},
+
+  playFirst: function() {
+
+
+  },
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -12,9 +12,14 @@ var AppModel = Backbone.Model.extend({
     end up referring to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
-
     params.library.on('play', function(song) {
       this.set('currentSong', song);
+    }, this);
+
+    params.library.on('enqueue', function(song){
+      currentSongQueue = this.get('songQueue')
+      currentSongQueue.add(song)
+      this.set('songQueue', currentSongQueue)
     }, this);
   }
 
